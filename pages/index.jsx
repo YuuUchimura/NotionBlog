05fe3a3306f6aecc58ddedb842958f6cab10ff93
Yuu <anpanmamZ3624@gmail.com>
2021-11-29 19:40:46 +0900

    ふわっと表示

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
 import { Nav } from '../components/Nav'
-import { Footer } from '../components/Footer'
 import { Container } from '../components/Container'
 
 export default function Home() {
@@ -7,12 +6,11 @@ export default function Home() {
     <>
       <Container>
         <Nav />
-        <div className="bg-background bg-no-repeat bg-cover fixed top-0 right-0 left-0 bottom-0 flex justify-center">
-          <h1 className="tracking-wider text-8xl mx-auto my-auto text-white font-serif">
-            YU UCHIMURA
+        <div className="opacity-90 bg-background bg-no-repeat bg-cover fixed top-0 right-0 left-0 bottom-0 flex justify-center">
+          <h1 className="animate-fade text-white tracking-wider text-8xl mx-auto my-auto font-serif">
+            Welcome
           </h1>
         </div>
-        {/* <Footer /> */}
       </Container>
     </>
   )
