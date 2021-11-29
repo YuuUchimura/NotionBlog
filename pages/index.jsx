53dffce271785f8c2229e715a15d77472f462a74
Yuu <anpanmamZ3624@gmail.com>
2021-11-29 20:08:05 +0900

    不要なclassの削除

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     <>
       <Container>
         <Nav />
-        <div className="opacity-90 bg-background bg-no-repeat bg-cover fixed top-0 right-0 left-0 bottom-0 flex justify-center">
+        <div className="opacity-90 bg-background bg-cover fixed top-0 right-0 left-0 bottom-0 flex justify-center">
           <h1 className="animate-fade text-white tracking-wider text-8xl mx-auto my-auto font-serif">
             Welcome
           </h1>
