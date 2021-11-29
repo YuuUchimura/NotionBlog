e143d1332991683b46fc05f0a0dd7ad74556b739
Yuu <anpanmamZ3624@gmail.com>
2021-11-29 14:05:29 +0900

    toppe-jino

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,14 +9,24 @@ const ExternalLink = ({ href, children }) => (
 
 export const Nav = () => {
   return (
-    <nav className="flex justify-between w-3/5 my-24 mx-auto items-center">
+    <nav className="flex justify-around items-center bg-black fixed left-0 right-0 top-0 z-10">
       <Link href="/">
         <Image src="/images/avater.png" width={84} height={84} />
       </Link>
-      <div className="flex justify-between w-36 items-end">
+      <div className="flex justify-between w-36 items-end text-white text-xl">
         <Link href="/about">About</Link>
         <Link href="/blog">Blog</Link>
       </div>
+      {/* <div>
+        <div className="">
+          <ExternalLink href="https://twitter.com/Yuu_19960912">
+            <img src="/images/twitter.svg" width={40} height={40} alt="twitter-logo" />
+          </ExternalLink>
+        </div>
+        <ExternalLink href="https://github.com/YuuUchimura">
+          <img src="/images/github-icon.svg" width={38} height={38} alt="github-logo" />
+        </ExternalLink>
+      </div> */}
     </nav>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false,
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        background: 'url(/images/background6.jpg)',
+      },
+    },
   },
   variants: {
     extend: {},
