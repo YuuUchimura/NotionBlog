34852e25d7b1278e0bffdf72ad3945797c0330fb
Yuu <anpanmamZ3624@gmail.com>
2022-01-12 11:33:16 +0900

    フッターの編集

--- components/atoms/Footer.jsx
+++ components/atoms/Footer.jsx
@@ -11,18 +11,20 @@ const ExternalLink = ({ href, children }) => (
 
 export const Footer = () => {
   return (
-    <footer className="flex items-end w-full py-4">
-      <p className="text-gray-100">&copy; 2021</p>
-      <div className="flex justify-end items-end w-10/12 mx-auto">
-        <div className="mr-4">
-          <ExternalLink href="https://twitter.com/Yuu_19960912">
-            <img src="/images/twitter.svg" width={40} height={40} alt="twitter-logo" />
-          </ExternalLink>
-        </div>
-        <div>
-          <ExternalLink href="https://github.com/YuuUchimura">
-            <img src="/images/skills/github-icon.svg" width={38} height={38} alt="github-logo" />
-          </ExternalLink>
+    <footer className="w-full py-4">
+      <div className="flex justify-between items-end mx-auto">
+        <p className="text-gray-100 flex">Copyright &copy; 2021</p>
+        <div className="flex justify-end items-end">
+          <div className="mr-4">
+            <ExternalLink href="https://twitter.com/Yuu_19960912">
+              <img src="/images/twitter.svg" width={40} height={40} alt="twitter-logo" />
+            </ExternalLink>
+          </div>
+          <div>
+            <ExternalLink href="https://github.com/YuuUchimura">
+              <img src="/images/skills/github-icon.svg" width={38} height={38} alt="github-logo" />
+            </ExternalLink>
+          </div>
         </div>
       </div>
     </footer>
