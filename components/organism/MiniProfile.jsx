4dcf619ca58c1cbec1c16d9c9fd0362c53698176
Yuu <anpanmamZ3624@gmail.com>
2022-01-12 10:21:50 +0900

    プロフィールの編集

--- components/organism/MiniProfile.jsx
+++ components/organism/MiniProfile.jsx
@@ -11,11 +11,11 @@ export const MiniProfile = () => {
         <Image
           className="rounded-full"
           src="/images/my_icon.png"
-          width={380}
-          height={380}
+          width={300}
+          height={300}
           alt="my_icon"
         />
-        <div className="flex flex-col mx-auto w-full font-medium">
+        <div className="flex flex-col mx-auto w-6/12 font-medium">
           <h2 className="text-4xl my-5">Yuu Uchimura</h2>
           <p>
             高校卒業後就職のため上京。約５年間勤めるもアプリがどうできているのか気になって調べながら作ってみると楽しくなり「これを仕事にしたい！」と思い転職を決意。
