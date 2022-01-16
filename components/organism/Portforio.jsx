e9c745ee85bf12a04a7581053adb774263d30910
Yuu <anpanmamZ3624@gmail.com>
2022-01-16 21:00:22 +0900

    不要なコメントの削除

--- components/organism/Portforio.jsx
+++ components/organism/Portforio.jsx
@@ -3,6 +3,7 @@ import { TitleAndNumber } from '../morecures/TitileAndNumber'
 import { ExternalLink } from '../atoms/Footer'
 import { Tag } from '../atoms/Tag'
 import { PortforioDesc } from '../atoms/PortforioDesc'
+//mapの要素
 // const pfs = {
 //   title: ['WEBサイト', 'SNAKE-GAME'],
 //   url: [
@@ -16,11 +17,8 @@ export const Portforio = () => {
   return (
     <div>
       <TitleAndNumber number="No.3" title="PORTFORIO" />
+      {/* 後にmapで回す */}
       <div className="lg:flex justify-around">
-        {/* {Object.keys(pfs).map((pf ,index) => {
-          {
-            console.log(pf)
-            return ( */}
         <div className="flex flex-col justify-between lg:w-5/12 w-full text-sm mb-12 p-5 text-white bg-black shadow-lg border-t-4 border-solid border-yellow-300">
           <h2 className="text-xl">WEBサイト</h2>
           <ExternalLink href="https://yuuuchimura.github.io/salon_beppu/">
@@ -49,8 +47,6 @@ export const Portforio = () => {
             <PortforioDesc description="ゲーム" />
           </div>
         </div>
-        {/* )}
-        })} */}
       </div>
     </div>
   )
