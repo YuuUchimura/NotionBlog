f6a7dbfb5443617a90ca54f9768affe39f29fc0b
Yuu <anpanmamZ3624@gmail.com>
2022-01-16 22:15:05 +0900

    フォントの編集

--- components/morecures/TitileAndNumber.jsx
+++ components/morecures/TitileAndNumber.jsx
@@ -1,8 +1,8 @@
 export const TitleAndNumber = ({ number, title }) => {
   return (
     <div className="flex justify-start items-end font-display animate-slideIn text-gray-100 text-6xl py-10">
-      <h1>{number}</h1>
-      <h1 className="ml-4 text-5xl">{title}</h1>
+      <h1 className="text-5xl md:text-7xl">{number}</h1>
+      <h1 className="ml-4 text-3xl md:text-6xl">{title}</h1>
     </div>
   )
 }
